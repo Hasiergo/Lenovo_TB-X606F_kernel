4001f8320a00717ab6f8d5303b2981048209243f
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2017-04-04 17:09:08 +0100

    efi/libstub: Unify command line param parsing
    commit 60f38de7a8d4e816100ceafd1b382df52527bd50 upstream.
    
    Merge the parsing of the command line carried out in arm-stub.c with
    the handling in efi_parse_options(). Note that this also fixes the
    missing handling of CONFIG_CMDLINE_FORCE=y, in which case the builtin
    command line should supersede the one passed by the firmware.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Matt Fleming <matt@codeblueprint.co.uk>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: bhe@redhat.com
    Cc: bhsharma@redhat.com
    Cc: bp@alien8.de
    Cc: eugene@hp.com
    Cc: evgeny.kalugin@intel.com
    Cc: jhugo@codeaurora.org
    Cc: leif.lindholm@linaro.org
    Cc: linux-efi@vger.kernel.org
    Cc: mark.rutland@arm.com
    Cc: roy.franz@cavium.com
    Cc: rruigrok@codeaurora.org
    Link: http://lkml.kernel.org/r/20170404160910.28115-1-ard.biesheuvel@linaro.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    [ardb: fix up merge conflicts with 4.9.180]
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/firmware/efi/libstub/efi-stub-helper.c
+++ drivers/firmware/efi/libstub/efi-stub-helper.c
@@ -32,6 +32,13 @@
 
 static unsigned long __chunk_size = EFI_READ_CHUNK_SIZE;
 
+static int __section(.data) __nokaslr;
+
+int __pure nokaslr(void)
+{
+	return __nokaslr;
+}
+
 /*
  * Allow the platform to override the allocation granularity: this allows
  * systems that have the capability to run with a larger page size to deal
@@ -365,6 +372,7 @@ efi_status_t efi_parse_options(char const *cmdline)
 	str = strstr(cmdline, "nokaslr");
 	if (str == cmdline || (str && str > cmdline && *(str - 1) == ' '))
 		__nokaslr = 1;
+<<<<<<< HEAD
 
 	/*
 	 * Currently, the only efi= option we look for is 'nochunk', which
@@ -373,6 +381,8 @@ efi_status_t efi_parse_options(char const *cmdline)
 	 */
 	if (!IS_ENABLED(CONFIG_X86))
 		return EFI_SUCCESS;
+=======
+>>>>>>> 8fca3c364683... efi/libstub: Unify command line param parsing
 
 	/*
 	 * If no EFI parameters were specified on the cmdline we've got
